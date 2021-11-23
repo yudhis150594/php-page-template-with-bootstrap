aa6079f5cdaccdf3fda527259c4f457d0c8f6609
Yudhistira Yoga Vacara <yudhistira1594yogavacara@gmail.com>
2021-11-23 21:22:43 +0700

    add pages

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # php-page-template-with-bootstrap
 PHP Page Template with Bootstrap
+
+## Git Commands
+
+Perintah untuk clonning repository
+I git clone <repo-url-here>
+
+I git add .
+I git commit -m "title of commit"
+I git push
---
+++ pages/courses.php
@@ -0,0 +1,12 @@
+ <!-- content -->
+ <div class="container">
+        <div class="row py-5">
+            <div class="col-lg-12">
+                <h1>Courses</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit voluptate eaque aperiam! Id natus temporibus blanditiis praesentium voluptates, delectus ratione numquam saepe sapiente! Esse, iste? Perferendis quaerat voluptatibus facere cum!
+                </p>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Impedit voluptate eaque aperiam! Id natus temporibus blanditiis praesentium voluptates, delectus ratione numquam saepe sapiente! Esse, iste? Perferendis quaerat voluptatibus facere cum!
+                </p>
+            </div>
+    </div>
+    <!-- end content -->
